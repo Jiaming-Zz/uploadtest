7c3a94b1174ecdcef58b6386747d8bec38cf4c84
Jiaming Zhang <jiaming.zhang@ZD-Automotive.de>
2023-02-21 17:31:00 +0100

    TEST

---
+++ zdc_version.h
@@ -0,0 +1,23 @@
+/**************************************************************************//**
+ * @file     zdc_version.h
+ * @version  V1.0
+ * $Revision: 10 $
+ * $Date: 2021/02/03 18:00 $
+ * @brief    ZD Converter 1500/2000 firmware version header file. 
+ *
+ * @copyright (C) 2021 ZD-Automotive GmbH. All rights reserved.
+ ******************************************************************************/
+
+#ifndef __ZDC_VERSION_H
+#define __ZDC_VERSION_H
+
+
+/*********************************** bootloader version ***************************************/
+#define ZDC_VERSION_MAJOR  1
+#define ZDC_VERSION_MINOR  9
+
+#define ZDC_VERSION_DATE   __DATE__
+#define ZDC_VERSION_TIME   __TIME__
+
+#endif /* __ZDC_VERSION_H */
+
